c88537d2d7a19c783af494f48ec716a0e80ce93b
GNF DEDSEC <ssuraw@gmail.com>
2023-06-29 12:27:38 +0700

    cotext search

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import MyContext from '@/src/context';
 
-export default function Index() {
-  const { value } = useContext(MyContext);
+export async function getStaticProps() {
+  const res = await fetch('https://dummyjson.com/products?limit=12');
+  const data = await res.json();
 
-  return (
-    <div>
-      <h1>Value from Navbar: {value}</h1>
-    </div>
-  );
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
+export default function Index({ products }) {
+  const { value } = useContext(MyContext);
+  const [searchValue, setSearchValue] = useState('');
+
+  // Filter the list with matching `value` from useContext and case-insensitive search
+  const filteredProducts = products.filter((item) =>
+    item.title.toLowerCase().includes(value.toLowerCase())||
+    item.brand.toLowerCase().includes(value.toLowerCase())
+  );
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value.toLowerCase());
+  };
+
+  return (
+    <>
+      <h1>Value from Navbar: {value}</h1>
+      <div>
+        <input
+          type="text"
+          value={searchValue}
+          onChange={handleSearchChange}
+          placeholder="Search"
+        />
+      </div>
+      <div>
+        {filteredProducts
+          .filter((item) => item.title.toLowerCase().includes(searchValue))
+          .map((item) => (
+            <div key={item.id}>
+              {item.id} {item.title} {item.brand}
+            </div>
+        ))}
+      </div>
+    </>
+  );
+}
